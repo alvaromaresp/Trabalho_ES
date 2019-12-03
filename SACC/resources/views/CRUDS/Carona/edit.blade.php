b0c6ec5787fd98776972adc83da05b45cb232f61
alvaromaresp <alvaromaresp@gmail.com>
2019-12-02 21:32:05 -0300

    fix edit names

--- SACC/resources/views/CRUDS/Carona/edit.blade.php
+++ SACC/resources/views/CRUDS/Carona/edit.blade.php
@@ -12,35 +12,27 @@
         <div class="infos-carro">
             <div class="inpItens">
                 <div>Destino:</div>
-                <input value="{{$carona->destino}}" class="input" type="text" name="destino" id="destino" placeholder="Destino" required/>
+                <input value="{{$carona->local}}" class="input" type="text" name="local" id="destino" placeholder="Destino" required/>
+            </div>
+            <div class="inpItens">
+                <div>Data:</div>
+                <input value="{{$carona->data}}" class="input" type="date" name="data" id="data" placeholder="Data" required/>
             </div>
             <div class="inpItens">
                 <div>Hora de saída:</div>
-                <input value="{{$carona->horaSaida}}" class="input" type="text" name="Hora de saída" id="Hora de saída" placeholder="Hora de saída" required/>
+                <input value="{{$carona->horario}}" class="input" type="text" name="horario" id="horario" placeholder="Hora de saída" required/>
             </div>
             <div class="inpItens">
-                <div>Previsão de chegada:</div>
-                <input value="{{$carona->previsaoChegada}}" class="input" type="text" name="Previsão de chegada" id="Previsão de chegada" placeholder="Previsão de chegada" required/>
+                <div>Duração:</div>
+                <input value="{{$carona->duracao}}" class="input" type="text" name="duracao" id="Duração" placeholder="Duração" required/>
             </div>
             <div class="inpItens">
                 <div>Carro:</div>
-                <select>
+                <select name="carro" class="form-control">
                     @foreach ($carros as $carro)
                         <option @if ($carro->id == $carona->carro) selected @endif value="{{$carro->id}}">{{$carro->marca}} - {{$carro->modelo}} - {{$carro->placa}}</option>
                     @endforeach
                 </select>
-            </div>
-            <div class="inpItens">
-                <div>Pessoas no carro:</div>
-                <input value="{{$carona->pessoasCarro}}" class="input" type="number" name="Pessoas no carro" id="Pessoas no carro" required/>
-            </div>
-            <div class="inpItens">
-                <div>Animais no carro:</div>
-                <input value="{{$carona->animaisCarro}}" class="input" type="number" name="Animais no carro" id="Animais no carro" required/>
-            </div>
-            <div class="inpItens">
-                <div>Lugar para mala:</div>
-                <input value="{{$carona->lugarMala}}" class="input" type="number" name="Lugar para mala" id="Lugar para mala" required/>
             </div>
         </div>
         @if(Session::has('msg'))
